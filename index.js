185c8c8c7ccea558f91831961bc6de3fa0ea6ad6
Sariputa Tiong <info@digitalfuture.melbourne>
2022-03-07 22:38:56 +1100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(name) {
+    let thankYouCard = []
+    let i = 0
+    for (i; i<name.length; i++) {
+        thankYouCard.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    }
+    return thankYouCard
+}
+
+function countDown(number) {
+    let i = number
+    for (i = number; i >= 0; i--){
+        console.log(i)
+    }
+}
